4542193bf004a8ecd5c7d1e1a9e1aa60dd4a8ce4
samswartz13 <samantha.r.swartz@gmail.com>
2015-08-29 00:32:58 -0400

    Add CSS Basics course & fleshed out iframe examples of cool code

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,1 +1,19 @@
-console.log('yay!!!!');
+var $container = $('#container');
+// init
+$container.packery({
+  itemSelector: '.item',
+  gutter: 10
+});
+
+$container.on( 'click', '.item', function( event ) {
+  var $target = $( event.target )
+  var isFullScreen = $target.hasClass('full-screen');
+  $target.toggleClass('full-screen');
+
+  if ( isFullScreen ) {
+    // if shrinking, just layout
+    $container.packery();
+  } else {
+    $container.packery( 'fit', event.target );
+  }
+});
