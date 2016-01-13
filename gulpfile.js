4410769debaf4948fd9008922cf80237b399218e
samswartz13 <samantha.r.swartz@gmail.com>
2016-01-13 00:12:23 -0500

    Started working on new Portfolio page

--- gulpfile.js
+++ gulpfile.js
@@ -1,149 +1,148 @@
 'use strict';
-var gulp        = require('gulp');
-var uglify      = require('gulp-uglify');
+
+var gulp         = require('gulp');
+var util         = require('gulp-util');
+var del          = require('del');
+var bower        = require('gulp-bower');
 
 // sass
-var sass        = require('gulp-ruby-sass');
-var filter      = require('gulp-filter');
-var sourcemap   = require('gulp-sourcemaps');
-var bower       = require('gulp-bower');
+var sass         = require('gulp-sass');
+var sourcemaps   = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
 
-var minifyCSS   = require('gulp-minify-css');
 // BrowserSync
-var browserSync = require('browser-sync');
-var reload      = browserSync.reload;
+var browserSync  = require('browser-sync').create();
 
-// HTML
-var minifyHTML  = require('gulp-minify-html');
-// js
+// Minification
+var minifyHTML   = require('gulp-minify-html');
+var uglify       = require('gulp-uglify');
+var minifyCSS    = require('gulp-minify-css');
 
 // image optimization
-var imagemin    = require('gulp-imagemin');
-var pngquant    = require('imagemin-pngquant');
+var imagemin     = require('gulp-imagemin');
+var pngquant     = require('imagemin-pngquant');
+
 
 var config = {
-     sassPath: './assets/scss',
-     bowerDir: './bower_components' 
+     sassPath: 'assets/scss',
+     bowerDir: 'bower_components' 
 }
-  // --------------------------
-  // BOWER INSTALL
-  // --------------------------
-  gulp.task('bower', function() { 
-    return bower()
+
+// --------------------------
+// BOWER INSTALL
+// --------------------------
+gulp.task('bower', function() { 
+	return bower()
          .pipe(gulp.dest(config.bowerDir)) 
-  });
+});
 
-  // --------------------------
-  // SASS
-  // --------------------------
-  gulp.task('icons', function() { 
-    return gulp.src(config.bowerDir + '/fontawesome/fonts/**.*') 
-        .pipe(gulp.dest('./assets/fonts')); 
-  });
-  gulp.task('sass', function() { 
-    return gulp.src(config.sassPath + '/custom.scss')
-         .pipe(sass({
-             style: 'expanded',
-             loadPath: [
-                 './assets/sass',
-                 config.bowerDir + '/bootstrap-sass/assets/stylesheets',
-                 config.bowerDir + '/fontawesome/scss'
-             ]
-         }) )
-         .pipe(gulp.dest('./assets/css'))
-  });
-/*  gulp.task('sass', function () {
-      return gulp.src('assets/scss/custom.scss')
-          .pipe(sass({sourcemap: true, style: 'compact'}))
-          .pipe(gulp.dest('assets/css'))// Write the CSS & Source maps
-          .pipe(filter('**.css')) // Filtering stream to only css files
-          .pipe(reload({stream:true}));
-  });*/
 
-  // --------------------------
-  // BUILD TASKS
-  // --------------------------
-	gulp.task('minifyHTML', function() {
-	    var opts = {comments:true,spare:true};
-	  gulp.src('*.html')
-	    .pipe(minifyHTML(opts))
-	    .pipe(gulp.dest('./build/'))
+// --------------------------
+// SASS
+// --------------------------
+gulp.task('icons', function() { 
+	gulp.src('bower_components/fontawesome/fonts/**.*') 
+	.pipe(gulp.dest('assets/fonts')); 
+});
+
+
+
+gulp.task('sass', function() {
+	return gulp.src("assets/scss/custom.scss")
+	.pipe(sourcemaps.init())
+	.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+
+	.pipe(autoprefixer({
+		browsers: ['last 2 versions'],
+		cascade: false
+	}))
+	.pipe(sourcemaps.write('.'))
+	.pipe(gulp.dest("assets/css"))
+
+	.pipe(browserSync.stream());
+});
+
+
+// --------------------------
+// BUILD TASKS
+// --------------------------
+
+gulp.task('minifyHTML', function() {
+	var opts = {comments:true,spare:true};
+	del(['build/*.html']);
+	gulp.src('*.html')
+	.pipe(minifyHTML(opts))
+	.pipe(gulp.dest('build/'))
+});
+
+gulp.task('minifyCSS', function() {
+	del(['build/assets/css/*']);
+	gulp.src('assets/css/*.css')
+	.pipe(minifyCSS({keepBreaks:true}))
+	.pipe(gulp.dest('build/assets/css'))
+});
+
+gulp.task('uglify', function() {
+	del(['build/assets/js/*']);
+	gulp.src('assets/js/*.js')
+	.pipe(uglify())
+	.pipe(gulp.dest('build/assets/js'))
+});
+
+gulp.task('minifyIMG', function () {
+	del(['build/assets/images/*']);
+	gulp.src('assets/images/*')
+	.pipe(imagemin({
+		progressive: true,
+		svgoPlugins: [{removeViewBox: false}],
+		use: [pngquant()]
+	}))
+	.pipe(gulp.dest('build/assets/images'));
+});
+
+// --------------------------
+// BROWSER SYNC
+// --------------------------
+gulp.task('browser-sync', function() {
+	browserSync.init({
+		server: "./"
 	});
 
-	gulp.task('minifyCSS', function() {
-	  gulp.src('./assets/css/*.css')
-	    .pipe(minifyCSS({keepBreaks:true}))
-	    .pipe(gulp.dest('./build/assets/css'))
+	gulp.watch("assets/scss/**/*.scss", ['sass']);
+	gulp.watch("*.html").on('change', browserSync.reload);
+});
+
+// gulp.task('reload-sass', ['sass'], function(){
+//   browserSync.reload();
+// });
+gulp.task('reload-js', function(){
+	browserSync.reload();
+});
+gulp.task('reload-html', function(){
+	browserSync.reload();
+});
+
+// --------------------------
+// DEV/WATCH TASK
+// --------------------------
+gulp.task('watch', [ 'sass', 'browser-sync'], function(){
+
+});
+
+// --------------------------
+// DEV/BUILD TASK
+// --------------------------
+gulp.task('build', [
+	'sass',
+	'minifyHTML',
+	'minifyCSS',
+	'minifyIMG',
+	'uglify'
+	], function() {
 	});
-	gulp.task('uglify', function() {
-	  gulp.src('./assets/js/*.js')
-	    .pipe(uglify())
-	    .pipe(gulp.dest('./build/assets/js'))
-	});
 
-  gulp.task('minifyIMG', function () {
-    return gulp.src('./assets/images/*')
-        .pipe(imagemin({
-            progressive: true,
-            svgoPlugins: [{removeViewBox: false}],
-            use: [pngquant()]
-        }))
-        .pipe(gulp.dest('./build/assets/images'));
-  });
 
-  // --------------------------
-  // BROWSER SYNC
-  // --------------------------
-  gulp.task('browser-sync', function() {
-      browserSync({
-          server: {
-              baseDir: "./"
-          },
-      });
-  });
- 
-  // gulp.task('reload-sass', ['sass'], function(){
-  //   browserSync.reload();
-  // });
-  gulp.task('reload-js', function(){
-    browserSync.reload();
-  });
-  gulp.task('reload-html', function(){
-    browserSync.reload();
-  });
+gulp.task('default', ['watch']);
 
-  // --------------------------
-  // DEV/WATCH TASK
-  // --------------------------
-  gulp.task('watch', [ 'sass', 'browser-sync'], function() {
-    // --------------------------
-    // watch:sass
-    // --------------------------
-    gulp.watch('./assets/scss/*.scss', ['sass']);
-    // --------------------------
-    // watch:js
-    // --------------------------
-    gulp.watch('./assets/js/*.js', ['reload-js']);
-    // --------------------------
-    // watch:html
-    // --------------------------
-    gulp.watch('./*.html', ['reload-html']);
-  });
-
-  // --------------------------
-  // DEV/BUILD TASK
-  // --------------------------
-  gulp.task('build', [
-    'sass', 
-    'minifyHTML', 
-    'minifyCSS', 
-    'minifyIMG', 
-    'uglify'
-    ], function() {
-  });
-
-   
-  gulp.task('default', ['watch']);
-   
-  // gulp (watch) : for development and browsersync
-  // gulp build : for a minified production build
+// gulp (watch) : for development and browsersync
+// gulp build : for a minified production build
